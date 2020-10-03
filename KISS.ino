aa5cd5a254c09f9e64e87be65e83df1e1c59f736
DonovanAdrian <websurfer21020@gmail.com>
2020-10-02 23:27:46 -0600

    Updated KISS File
    Welp, I ran into a... "couple" issues. In the meantime, here's a simplified version of the file. I should note that this does NOT currently work the way I intend it to, hence the simplification. Once I find the time, I will be properly researching how to work with arrays on here, lol.

--- KISS.ino
+++ KISS.ino
@@ -4,139 +4,46 @@
    KISS Version 1.0
 
    Keep It Simple... Silly! Stay busy with these daily activity suggestions 
-   and greetings. Follow the suggested Arduino Setup below to plug in the 
-   wires and components for the system!
-
-   
-
-   Required Components:
-   Liquid Crystal Display (LCD)
-
-   Optional Components:
-  2 LEDs (Any Color)
-	 -1 used as an indicator to show that the system is online
-   -1 used to show "processing"
-  2 Buttons
-	 -1 to reload (A)
-	 -1 to "re-roll" suggestion (B)
-
-  
-
-   Suggested Arduino Setup (UNFINISHED):
-   ooooo ggggg o
- 0 ***** ***** o
- 1 ***** ***** o
- 2 ***** ***** o
- 3 ***** ***** o
- 4 ***** ***** o
- 5 ***** ***** o
- 6 ***** ***** o
- 7 ***** ***** o
- 8 ***** ***** o
- 9 ***** ***** o
-10 ***** ***** o
-11 ***** ***** o
-12 ***** *****
-13 ***** *****
- g ***** *****
-AR ***** *****
-   ***** *****
-   ***** *****
-
-  *Based upon Arduino Prototype Shield
+   and greetings.
 
   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 */
 
 #include <LiquidCrystal.h>
-#define ARRAYSIZEA 82
-#define ARRAYSIZEB 101
 
-LiquidCrystal lcd(5, 4, 3, 2, 1, 0);
-const int buttonPinA = 7;
-const int buttonPinB = 8;
-const int ledPinOnline = 9;
-const int ledPinLoading = 10;
+LiquidCrystal lcd(0, 1, 10, 11, 12, 13);
 const int LCDInterval = 500;
 const int threeSeconds = 3000;
-const int oneHour = 3600000;
 const int twentyFourHours = 86400000;
-
-unsigned long previousDailyMillis = 0;
-unsigned long previousLCDMillis = 0;
-unsigned long currentMillis = 0;
-
 String todaysMessage = "";
 
 void setup() {
    lcd.begin(16, 2); //Initialize LCD
    lcd.print("  Welcome!  "); //Display Welcome
-
-   randomSeed(analogRead(0));
-   
-   pinMode(ledPinOnline, OUTPUT); //Initialize green light
-   pinMode(ledPinLoading, OUTPUT); //Initialize red light
-   pinMode(buttonPinA, INPUT); //Initialize Reload Btn
-   pinMode(buttonPinB, INPUT); //Initialize Reroll Btn
-    
-   digitalWrite(ledPinOnline, HIGH); //Turn on green light
-   digitalWrite(ledPinLoading, HIGH); //Turn on red light
    
    delay(threeSeconds); //Delay for 3 seconds
-   dailyMessage();
-}
-
-void reInitialize() {
-   lcd.begin(16, 2); //Initialize LCD
-   lcd.print("  Welcome!  "); //Display Welcome
-   
-   pinMode(ledPinOnline, OUTPUT); //Initialize green light
-   pinMode(ledPinLoading, OUTPUT); //Initialize red light
-   pinMode(buttonPinA, INPUT); //Initialize Reload Btn
-   pinMode(buttonPinB, INPUT); //Initialize Reroll Btn
-    
-   digitalWrite(ledPinOnline, HIGH); //Turn on green light
-   digitalWrite(ledPinLoading, HIGH); //Turn on red light
-   
-   delay(threeSeconds); //Delay for 3 seconds
-   dailyMessage();
 }
 
 void loop() {
-   currentMillis = millis();
-
-   readButton();
-   checkDailyUpdate();
-}
-
-void readButton(){
-   if (currentMillis - previousLCDMillis >= LCDInterval) {
-      if (digitalRead(buttonPinA) == LOW)
-         reInitialize();
-      if (digitalRead(buttonPinB) == LOW)
-         dailyMessage();
-      previousLCDMillis += LCDInterval;
-   }
-}
-
-void checkDailyUpdate(){
-  if(currentMillis - previousDailyMillis >= twentyFourHours) {
-    dailyMessage();
-    previousDailyMillis += twentyFourHours;
-  }
+   dailyMessage();
+   delay(twentyFourHours);
 }
 
 void dailyMessage() {
-   digitalWrite(ledPinOnline, LOW); //turn off green light
-   digitalWrite(ledPinLoading, HIGH); //turn on red light
    setTodaysMessage();
-   digitalWrite(ledPinOnline, HIGH); //turn on green light
-   digitalWrite(ledPinLoading, LOW); //turn off red light
+   
+   lcd.setCursor(0,0);
+   lcd.print("  TADA!!!!!  ");
 }
 
 void setTodaysMessage() {
+   lcd.setCursor(0,0);
+   lcd.print("  Hooray??  ");
+
+   //Not getting any further than this
+  
    int randomNum = 0;
-   String messages[ARRAYSIZEA] = {"Do Smthn New", "Take A Walk", 
+   String messages[] = {"Do Smthn New", "Take A Walk", 
                  "Try Yoga",     "Take A Hike",  "Exercise 15m",
                  "Exercise 30m", "Go Swimming",  "Read A Book",
                  "Fancy Dinner", "Mini Golf",    "Bowling",
@@ -165,7 +72,7 @@ void setTodaysMessage() {
                  "Make Bouquet", "Stargaze",     "Bath Bomb",
                  "Take A Bath",  "Watch Clouds"}; //82
 
-   String greetings[ARRAYSIZEB] = {"Hi There!", "Howdy!", 
+   String greetings[] = {"Hi There!", "Howdy!", 
                  "Good Day!",    "Nice 2 C U!",  "What's New?",
                  "How Goes It?", "Greetings!",   "Salutations!",
                  "What's Up?",   "Hey!",         "Hello!",
@@ -200,24 +107,28 @@ void setTodaysMessage() {
                  ";D",           ";D",           "<3",
                  "<3 <3",        "<3 <3 <3",     "<3 <3 <3 <3"};//101
 
+   lcd.setCursor(0,0);
+   lcd.print("  Hooray!!  ");
+
+
    randomNum = random(2);
    if(randomNum == 0){
-      randomNum = random(ARRAYSIZEA + 1);
+      randomNum = random(83);
       todaysMessage = messages[randomNum];
       
-      lcd.setCursor(1, 1);
+      lcd.setCursor(0, 0);
       lcd.print("To Do Today:");
    } else {
-      randomNum = random(ARRAYSIZEB + 1);
+      randomNum = random(102);
       todaysMessage = greetings[randomNum];
       
-      lcd.setCursor(1, 1);
+      lcd.setCursor(0, 0);
       lcd.print(todaysMessage);
       
-      randomNum = random(ARRAYSIZEB + 1);
+      randomNum = random(102);
       todaysMessage = greetings[randomNum];
    }
    
-   lcd.setCursor(2, 1);
+   lcd.setCursor(0, 1);
    lcd.print(todaysMessage);
 }
